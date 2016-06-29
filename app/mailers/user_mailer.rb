a31d13ba90da4cf02bd2f7bc108d16f6fb00301e
Alexander Munck af Rosenschöld <alexandermunckafrosenschold@Alexanders-MBP.lan>
2016-06-29 18:13:29 +0200

    Add welcome mail to new users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,10 @@ class UserMailer < ApplicationMailer
     :to => 'your-email@example.com',
     :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  @appname = "Spintailz"
+  mail( :to => user.email,
+        :subject => "Welcome to #{@appname}!")
+end
 end
