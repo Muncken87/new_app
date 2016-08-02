e37047521973a08a9abad619c2d86d49d7a2465a
Alexander <yumako87@gmail.com>
2016-08-02 14:49:03 +0200

    Add user rspec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,32 +3,32 @@ require 'rails_helper'
 
 describe UsersController, :type => :controller do
   before do
-      @user = FactoryGirl.create(:user)
+      @user1 = FactoryGirl.create(:user)
       @user2 = FactoryGirl.create(:user)
   end
 
   describe "GET #show" do
     context "User is logged in" do
       before do
-        sign_in @user
+        sign_in @user1
       end
   end
 
   it "Load User 1 details" do
-    get :show, id: @user.id
-    expect(response).to have_http_status(302)
-    expect(assigns(:user)).to eq @user
+    get :show, id: @user1.id
+    expect(assigns(:user)).to eq @user1
   end
 
   it "Doesnt allow user 1 to view users 2 details" do
   get :show, id: @user2.id
   expect(response).to have_http_status(302)
   expect(assigns(:user)).to eq @user2
+  expect(response).to redirect_to(root_path)
   end
 
 it "should not validate users without an email address" do
-  @user = FactoryGirl.build(:user, email: "")
-  expect(@user).to_not be_valid
+  @user1 = FactoryGirl.build(:user, email: "")
+  expect(@user1).to_not be_valid
   end
 
     # context "No user is logged in" do
