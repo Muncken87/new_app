d8be251aa3da434967a19ec44866f8fe22f820ce
Alexander <yumako87@gmail.com>
2016-08-04 18:43:43 +0200

    Add uglify to make angular work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
   config.app_generators.javascript_engine = :javascript
+  config.assets.js_compressor = Uglifier.new(mangle: false)
 
   # Show full error reports.
   config.consider_all_requests_local = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "new_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.assets.js_compressor = Uglifier.new(mangle: false)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -84,6 +85,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # Heroku variables
-  config.web_socket_server_url = "https://spintailz.herokuapp.com"
+  config.web_socket_server_url = "https://spintailz.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['https://spintailz.herokuapp.com/', 'https://spintailz.herokuapp.com/']
 end
